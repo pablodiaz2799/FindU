62139738a29a4890eb5b5f02ece0febd570d4f5f
Pablo Diaz <pablo.diaz2799@gmail.com>
2019-08-13 19:33:04 -0300

    Home component created

--- Workers/src/app/app.module.ts
+++ Workers/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'signup', component: SignupComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
